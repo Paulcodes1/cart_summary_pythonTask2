7cb8a4794adbe1ecdc428945f1712988b7266112
Paul Ofili <paulcodes001@gmail.com>
2025-01-17 16:15:48 +0100

    Created the global variables, understood the *args and **kwargs parameters

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # Program that simulates a shopping cart
 
-def add_to_cart(item_name, price, *args, **kwargs)
+item_name = input("Enter item name(or 'done' to finish): ")
+price = input("Enter item price: ")
+discounts = input("Enter discounts(if any, separated by spaces): ").split(" ")
+item_details = input("Enter item details(e.g, color=red size=large): ")
+
+def add_to_cart(item_name, price, *discounts, **item_details) -> str :
+    #print("---Cart Summary---")
+    
+    return (item_name, price, discounts, item_details)
+
+cart = add_to_cart(item_name, price, discounts, item_details)
+print(cart)
